442612dc2c3dcbfb5a5e65d499af9d9833c6ca55
ThunderSword99 <guardian.of.gods99@gmail.com>
2020-07-12 14:47:11 +0700

    Initial commit

---
+++ Assets/Scripts/Ceiling.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ceiling : MonoBehaviour
+{
+    public float speed = 4;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.transform.Rotate(0f,0f,speed);
+    }
+}
