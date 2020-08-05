e37a810b1f6811099f5334460c9a1997bdca23f7
ThunderSword99 <guardian.of.gods99@gmail.com>
2020-08-05 13:05:12 +0700

    Able to open door, turn light, turn celing fan

--- Assets/Scripts/Ceiling.cs
+++ Assets/Scripts/Ceiling.cs
@@ -4,12 +4,41 @@ using UnityEngine;
 
 public class Ceiling : MonoBehaviour
 {
-    public float speed = 4;
+    public float speed = 0;
+    public float maxspeed = 50;
+    private bool isTurnOn = false;
 
+    private bool isCeiling()
+    {
+        return speed>0;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (speed>maxspeed)
+        {
+            speed = maxspeed;
+        }
+        if (speed<0)
+        {
+            speed=0;
+        }
+
+        if (isTurnOn)
+        {
+            speed += Time.deltaTime * 4;
+        }
+        else if (!isTurnOn && isCeiling())
+        {
+            speed -= Time.deltaTime * 4;
+        }
+
         this.transform.Rotate(0f,0f,speed);
+    }
+
+    public void TurnOnOff()
+    {
+        isTurnOn = !isTurnOn;
     }
 }
---
+++ Assets/Scripts/RaycastHitObject.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastHitObject : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (hit.collider.CompareTag("Switcher"))
+                {
+                    GameObject thing = hit.collider.GetComponent<Switcher>().thing;
+                    if (thing.CompareTag("CeilingFan"))
+                    {
+                        thing.GetComponent<Ceiling>().TurnOnOff();
+                    }
+                    if (thing.CompareTag("Light"))
+                    {
+                        if (thing.activeSelf)
+                        {
+                            thing.SetActive(false);
+                        }
+                        else
+                        {
+                            thing.SetActive(true);
+                        }
+                    }
+                }
+                if (hit.collider.CompareTag("Door"))
+                {
+                    GameObject thing = hit.collider.GetComponent<DoorHinge>().doorHinge;
+                    if (thing.transform.rotation.z == 0)
+                    {
+                        thing.transform.Rotate(0,0,90);
+                    }
+                    else
+                    {
+                        thing.transform.Rotate(0,0,-90);
+                    }
+                    
+                }
+            }
+        }
+    }
+}
